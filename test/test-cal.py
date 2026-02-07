1e4d12f71d3e26286205196bbf325411d09adc35
kaushikpoddar14 <166856471+kaushikpoddar14@users.noreply.github.com>
2026-02-07 22:06:52 +0530

    Implement tests for add and subtract functions
    Add test cases for add and subtract functions.

---
+++ test/test-cal.py
@@ -0,0 +1,12 @@
+from app.cal.py import add,substract
+
+'''
+from app--> folder
+calculator --> filename
+import --> add,substract(functions)
+'''
+
+def test_add():
+  assert add(2,3) == 5
+
+def test_substract(5,3) == 2
